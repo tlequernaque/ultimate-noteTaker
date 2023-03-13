e0c98dce77a8c976b0c5082d96bbc076de7faeb4
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-03-12 23:18:05 -0400

    added code to server.js

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const fs = require("fs");
 const path = require('path');
 const PORT = process.env.port || 3001;
-
+const api = require('./public/assets/js/index');
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
@@ -10,4 +15,17 @@ app.get('/notes', (req, res) =>
 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/index.html'))
+);
+
+
+app.get('/api/notes', (req, res) => {
+    res.status(200).json(reviews);
+});
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
 );
