9d73f08bb7eb65a8625f4e5e7e94261695987ff3
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-03-13 00:33:30 -0400

    changed port for heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const fs = require("fs");
 const path = require('path');
-const PORT = process.env.port || 3008;
+const PORT = process.env.port || 3001;
 const api = require('./public/assets/js/index');
 const app = express();
 const { v4: uuidv4 } = require('uuid');
