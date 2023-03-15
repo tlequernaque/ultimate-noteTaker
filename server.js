bad813250f89d857a672d6ba4e5fec440eedd9f6
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-03-15 01:49:00 -0400

    changed code to maker server work

--- server.js
+++ server.js
@@ -2,22 +2,17 @@ const express = require('express');
 const fs = require("fs");
 const path = require('path');
 const PORT = process.env.port || 3001;
-const api = require('./public/assets/js/index');
+// const api = require('./public/assets/js/index');
 const app = express();
 const { v4: uuidv4 } = require('uuid');
 
-app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use('/api', api);
+app.use(express.json());
+app.use(express.static("public"));
 
 app.get('/notes', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
-
-app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/index.html'))
-);
-
 
 app.get('/api/notes', (req, res) => {
     const dataNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf-8");
@@ -34,6 +29,10 @@ app.post('/api/notes', (req, res) => {
     fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(parseNotes), "utf-8");
     res.json("You have added a note!");
 });
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
 // app.delete('/api/notes/:id', (req, res) => {
 //     res.send("DELETE Request Called")
