9e904f1b5c07f1f899da4da4c161f02d37a79073
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-03-16 01:17:06 -0400

    finished server.js

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
 const express = require('express');
-
-const path = require('path');
-const PORT = process.env.port || 3001;
-// const api = require('./public/assets/js/index');
 const app = express();
+const PORT = process.env.port || 3001;
 
+const apiRoute = require('./routes/apiRoute');
+const htmlRoute = require('./routes/htmlRoute');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-
-
+app.use('/api', apiRoute);
+app.use('/', htmlRoute)
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)
