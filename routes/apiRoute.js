c4981897fb4f770b3b3ad2b205f3d6da4d1c1cdd
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-03-16 01:58:52 -0400

    added some code

--- routes/apiRoute.js
+++ routes/apiRoute.js
@@ -5,13 +5,13 @@ const { v4: uuidv4 } = require('uuid');
 const note = require('../db/db.json');
 
 
-router.get('./notes', (req, res) => {
-    const dataNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf-8");
+router.get('/notes', (req, res) => {
+    const dataNotes = fs.readFileSync(path.join(__dirname, '../db/db.json'), "utf-8");
     const parseNotes = JSON.parse(dataNotes);
     res.json(parseNotes);
 });
 
-router.post('./notes', (req, res) => {
+router.post('/notes', (req, res) => {
     const dataNotes = fs.readFileSync(path.join(__dirname, '../db/db.json'), "utf-8");
     const parseNotes = JSON.parse(dataNotes);
     req.body.id = uuidv4()
@@ -21,7 +21,7 @@ router.post('./notes', (req, res) => {
     res.json("You have added a note!");
 });
 
-router.delete('./notes/:id', (req, res) => {
+router.delete('/notes/:id', (req, res) => {
     for (let i = 0; i < note.length; i++) {
         if (note[i].id === req.params.id) {
             note.splice(i, 1);
