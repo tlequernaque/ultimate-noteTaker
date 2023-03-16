fc7d316b8a76e360f0a44a6637cee1984179573b
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-03-15 22:12:43 -0400

    fixed code

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-app.get('/notes', (req, res) =>
+app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, '/public/notes.html'))
 );
 
