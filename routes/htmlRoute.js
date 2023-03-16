af4574f900c5eff368a1467c593606aba00fb59a
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-03-16 00:47:56 -0400

    added routes folder with routes js, started html route code

---
+++ routes/htmlRoute.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+);
+
+app.get('/', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/index.html'))
+);
--- server.js
+++ server.js
@@ -10,25 +10,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-// app.get('/notes', (req, res) =>
-//   res.sendFile(path.join(__dirname, './public/notes.html'))
-// );
+app.get('/notes', (req, res) =>
+  res.sendFile(path.join(__dirname, './public/notes.html'))
+);
 
-// app.get('/api/notes', (req, res) => {
-//     const dataNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf-8");
-//     const parseNotes = JSON.parse(dataNotes);
-//     res.json(parseNotes);
-// });
+app.get('/api/notes', (req, res) => {
+    const dataNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf-8");
+    const parseNotes = JSON.parse(dataNotes);
+    res.json(parseNotes);
+});
 
-// app.post('/api/notes', (req, res) => {
-//     const dataNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf-8");
-//     const parseNotes = JSON.parse(dataNotes);
-//     req.body.id = uuidv4()
-//     parseNotes.push(req.body);
+app.post('/api/notes', (req, res) => {
+    const dataNotes = fs.readFileSync(path.join(__dirname, './db/db.json'), "utf-8");
+    const parseNotes = JSON.parse(dataNotes);
+    req.body.id = uuidv4()
+    parseNotes.push(req.body);
 
-//     fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(parseNotes), "utf-8");
-//     res.json("You have added a note!");
-// });
+    fs.writeFileSync(path.join(__dirname, './db/db.json'), JSON.stringify(parseNotes), "utf-8");
+    res.json("You have added a note!");
+});
 
 app.get('/', (req, res) =>
   res.sendFile(path.join(__dirname, './public/index.html'))
