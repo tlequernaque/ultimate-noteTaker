7ae5510121d6bfce90b3c5f1b9ef9557df13def0
Terry Lequernaque <t.lequernaque@yahoo.com>
2023-03-15 22:19:36 -0400

    changed code

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 app.get('/', (req, res) =>
-  res.sendFile(path.join(__dirname, '/public/notes.html'))
+  res.sendFile(path.join(__dirname, './public/notes.html'))
 );
 
 app.get('/api/notes', (req, res) => {
